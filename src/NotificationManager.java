a0a4fd360aa3c7f0d131698f8fe1cc68c81a165f
Addada Sanjay Varma <asanjayvarma18@gmail.com>
2024-12-11 11:47:34 +0530

    Update NotificationManager.java

--- src/NotificationManager.java
+++ src/NotificationManager.java
@@ -17,7 +17,7 @@ public class NotificationManager {
         if (enabledNotifications) {
             String units = data.getUserPreferences().getPreferredUnits();
             
-            // Temperature thresholds for Celsius and Fahrenheit
+            // Temperature thresholds for Celsius and Fahrenheit -> Below or Above this gon give alert
             double highTempThreshold = units.equals("imperial") ? 95.0 : 35.0;
             double lowTempThreshold = units.equals("imperial") ? 32.0 : 0.0;
 
